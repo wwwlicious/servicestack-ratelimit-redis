3ae9c67ca878df762f523ea60fe25243acbd0af0
Donald Gray <donald.gray@maclean.co.uk>
2016-04-21 16:50:01 +0100

    Added a maybe<> class

---
+++ src/ServiceStack.RateLimit.Redis/Utilities/Maybe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ServiceStack.RateLimit.Redis.Utilities
+{
+    /// <summary>
+    /// Used to specify that a class may return null object in certain instances
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <remarks>This is a naive implementation as it's not widely used in this instance</remarks>
+    public struct Maybe<T>
+        where T : class
+    {
+        private readonly T value;
+
+        public T Value
+        {
+            get
+            {
+                if (!HasValue)
+                    throw new InvalidOperationException("Nullable object must have a value");
+
+                return value;
+            }
+        }
+
+        public bool HasValue => value != null;
+
+        public Maybe(T value)
+        {
+            this.value = value;
+        }
+    }
+}
